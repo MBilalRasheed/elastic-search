af88d089c12785c094398ccf86172d01eaf4c524
MudassarSharifCB <mudassar.sharif@crowdbotics.com>
2023-03-01 17:26:50 +0500

    code cleaning

--- Backend/controller/Roles.js
+++ Backend/controller/Roles.js
@@ -2,20 +2,14 @@ const client = require("../elasticConfig/index.js");
 
 function searchRoles(req, res, next) {
     try {
-        {
-            console.log('searchRoles');
-            console.log(req.body);
-            client.search({
-                body: req.body,
-                index: 'roledefs'
-            })
-                .then((e) => {
-                    console.log(e); res.json(e)
-                })
-                .catch((c) => {
-                    console.log(c); res.json(c)
-                });
-        }
+        client.search({
+            body: req.body,
+            index: 'roledefs'
+        }).then((e) => {
+            console.log(e); res.json(e)
+        }).catch((c) => {
+            console.log(c); res.json(c)
+        });
     } catch (err) {
         console.log(err.message);
     }
